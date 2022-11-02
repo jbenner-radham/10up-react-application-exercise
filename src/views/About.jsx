e4ee1260919ffd83dfea01e0fb68429c5a5530d5
James Benner <james.benner@gmail.com>
2022-11-02 14:29:26 -0400

    Fetch and display about text

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import DOMPurify from 'dompurify';
+import React, { useEffect, useState } from 'react';
 
 function About() {
+    const [about, setAbout] = useState(null);
+
+    async function getPages() {
+        const url = 'https://js1.10up.com/wp-json/wp/v2/pages';
+        const options = {
+            mode: 'cors'
+        };
+        const response = await fetch(url, options);
+        const pages = await response.json();
+
+        return pages;
+    }
+
+    useEffect(() => {
+        getPages().then(pages => {
+            const aboutUsPage = pages.find(page => page?.slug === 'about-us');
+
+            setAbout(aboutUsPage?.content?.rendered);
+        });
+    }, []);
+
     return (
         <>
             <h1>
                 About
             </h1>
 
-            <div className="page">
+            <div className="page" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(about) }}>
                 {/* Retrieve about text from WP API. */}
-                10up about text...
             </div>
         </>
     );
